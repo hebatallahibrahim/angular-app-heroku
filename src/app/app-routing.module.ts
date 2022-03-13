8bfc710cbea10d8d52027078a560f831bc7d6601
Nada <nadaua2@gmail.com>
2022-03-13 16:25:30 +0200

    update about us page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -37,7 +37,7 @@ const routes: Routes = [
  
   { path: 'blogs', component: BlogsPageComponent },
   { path: 'contactUs', component: ContactUsComponent },
-  { path: 'aboutUs', component: ContactUsComponent }, // add about us component
+  { path: 'aboutUs', component: AboutUsComponent }, // add about us component
   { path: 'forgetPass', component: ForgetPasswordComponent },
   { path: 'cart',canActivate:[AuthGuard] ,component: CartComponent },
  
