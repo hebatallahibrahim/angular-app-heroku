822de70f370dcf274d56aaf53577975ee0859d5d
hebatallah ibrahim <heba_walid1012@gmail.com>
2022-04-06 00:00:48 +0200

    first

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-const indexPath = path.join(__dirname, './index.html')
-const distPath = express.static(path.join(__dirname, './dist'))
+app.use(express.static('./dist/homesfurniture'));
 
-app.use('/dist', distPath)
-app.get('/', function (req, res) { res.sendFile(indexPath) }) 
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/homesfurniture/'}),
+);
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
