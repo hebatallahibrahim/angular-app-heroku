3ac1122ea51aee170d75ede6d100a3533d229259
hebatallah ibrahim <heba_walid1012@gmail.com>
2022-04-05 23:33:40 +0200

    first

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/E-commers-furniture_-ITI-Project'));
+app.use(express.static('./downloads/final-Project Angular/New folder (2)/E-commers-furniture_-ITI-Project'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/E-commers-furniture_-ITI-Project/'}),
+    res.sendFile('index.html', {root: '/downloads/final-Project Angular/New folder (2)/E-commers-furniture_-ITI-Project/'}),
 );
 
 // Start the app by listening on the default Heroku port
